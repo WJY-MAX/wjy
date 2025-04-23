f5578e789356c9692e72e9f5dd00e47687f189bd
WJY-MAX <3559245329@qq.com>
2025-04-23 09:34:49 +0800

    Add files via upload

---
+++ pythonProject/111.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# 404 错误处理
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
